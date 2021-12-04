9de40f3f16e068888f8edf588409301bf4b738ac
Myriamchtf <myriamsara94@gmail.com>
2021-12-04 19:43:54 +0100

    Variable, constructeur , getter et la méthode getListOfPossibleOri()

--- src/main/java/fr/dauphine/JavaAvance/Components/PieceType.java
+++ src/main/java/fr/dauphine/JavaAvance/Components/PieceType.java
@@ -11,19 +11,29 @@ import java.util.LinkedList;
  */
 public enum PieceType {
 	// Each Type has a number of connectors and a specific value
-//Enlever les (i) comme pour Orientation !!!!!!	
-VOID(0),
-ONE_NEIGHBOUR(1),
-BAR(2),
-T(3),
-CROSS(4),
-L(5),;
+
+	VOID(0,0)//Empty
+	, ONECONN(1,1) //only 1 neighbour
+	, BAR(2,2) // 2 opposite neighbours.
+	,TTYPE(3,3) //seeing 3 neighbours (a T)
+	,FOURCONN(4,4) // seeing 4 neighbours (a cross).
+	,LTYPE(2,5) //seeing 2 consecutive neighbours (an L)
+	; 
 	
+	private int nbOfConnectors;
+	private int SpecificValue;
+	
+	PieceType(int nbOfConnectors, int SpecificValue) {
+        this.nbOfConnectors = nbOfConnectors;
+        this.SpecificValue = SpecificValue;
+    }
+	
+	
+	 public int getNbConnectors() { return nbOfConnectors; } //accèder au nombre de connecteur
+	 public double SpecificValue() { return SpecificValue; }
 
 
-PieceType(int i) {
-	// TODO Auto-generated constructor stub
-}
+	
 
 /**
  * getter de l orientation 
@@ -43,12 +53,69 @@ LinkedList<Orientation> setConnectorsList(Orientation north) {
 }
 /**
  * 
- * @return
+ *  @return Orientation possible pour une piece
  */
-ArrayList<Orientation> getListOfPossibleOri() {
-	// TODO Auto-generated method stub
-	return null;
-}
+ArrayList<Orientation> getListOfPossibleOri() {	
+	 ArrayList<Orientation> O = new ArrayList<Orientation>();
+	 
+	 Orientation n = Orientation.NORTH;
+	 Orientation e = Orientation.EAST;
+	 Orientation s = Orientation.SOUTH;
+	 Orientation w = Orientation.WEST;		
+	 
+	 PieceType piecetype = PieceType.VOID;
+	 
+	 switch(piecetype) {
+	 	case VOID :
+	 		O.add(null); 
+	 		break;
+	 	case ONECONN :
+	 	case TTYPE :
+	 	case LTYPE :
+	 		O.add(n); O.add(e); O.add(s); O.add(w);
+	 		break;
+	 	case BAR : 
+			 O.add(n); O.add(e);
+			 break;
+	 	case FOURCONN : 
+			O.add(n);
+			break;	
+		default :
+			throw new IllegalArgumentException("Ce type de pièce n'existe pas");
+	 	}
+	 	return O;
+	 }
+
+	 /*for (PieceType type : PieceType.values()) {
+		 			 
+		 	if (type == VOID) {
+				O.add(null); 
+				}
+			
+			if (type == ONECONN) {
+				O.add(n); O.add(e); O.add(s); O.add(w);
+				}
+			
+			if (type == BAR) {
+				 O.add(n); O.add(e);
+			}
+			
+			if (type == TTYPE) {
+				O.add(n); O.add(e); O.add(s); O.add(w);
+				}
+			
+			if (type == FOURCONN) {
+				O.add(n);
+				}
+			
+			if (type == LTYPE) {
+				O.add(n); O.add(e); O.add(s); O.add(w);
+				}
+			
+			}
+
+	return O;
+}*/
 /**
  *  Selon moi, cette methode recoit @param typeValue
  *  et en fct de sa valeur,
