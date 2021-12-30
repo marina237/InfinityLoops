ed9affb29268fc0df8a5607a76fcaa686b292504
Myriamchtf <myriamsara94@gmail.com>
2021-12-30 22:05:29 +0100

    Update Checker ( buildGrid et IsSolution) mais pas encore rempli les méthodes

--- src/main/java/fr/dauphine/JavaAvance/Solve/Checker.java
+++ src/main/java/fr/dauphine/JavaAvance/Solve/Checker.java
@@ -2,12 +2,39 @@ package fr.dauphine.JavaAvance.Solve;
 
 
 
+import java.io.IOException;
+
 import fr.dauphine.JavaAvance.Components.Piece;
 import fr.dauphine.JavaAvance.GUI.Grid;
 
 
 public class Checker {
+	
+	// To be implemented
+	
 
-// To be implemented
+	/**
+	 * 
+	 * @param inputFile          
+	 * @throws IOException
+	 *         
+	 */
+
+	public static Grid buildGrid(String inputFile) throws IOException {
+		return null;
+	}
+	
+	/**
+	 * 
+	 * @param inputFile          
+	 * @throws IOException
+	 * @return True if all the piece are connected, else it will return False
+	 * If all piece are connected it means that the game is solved
+	 */
+	public static boolean isSolution(String inputFile) throws IOException {
+		return false;
+		
+	}
+
 	
 }
