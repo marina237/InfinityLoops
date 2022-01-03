6a7fe8e624ca6e5dabc11cca5edb0ec98410b672
Myriamchtf <myriamsara94@gmail.com>
2022-01-03 13:45:23 +0100

    Update Checker (commencé à implémenter buildgrid et issolution mais pas fini)

--- src/main/java/fr/dauphine/JavaAvance/Solve/Checker.java
+++ src/main/java/fr/dauphine/JavaAvance/Solve/Checker.java
@@ -2,6 +2,8 @@ package fr.dauphine.JavaAvance.Solve;
 
 
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 
 import fr.dauphine.JavaAvance.Components.Piece;
@@ -12,16 +14,32 @@ public class Checker {
 	
 	// To be implemented
 	
-
 	/**
-	 * 
+	 * Generate a grid based on the provided solution in the file
 	 * @param inputFile          
 	 * @throws IOException
 	 *         
 	 */
+	
+	//on doit retourner SOLVED : true et SOLVED : false dans standard output : normalement fait 
 
 	public static Grid buildGrid(String inputFile) throws IOException {
-		return null;
+		//generate a grid based on the provided solution in the file (on doit lire le fichier créer dans generator?)
+		
+		FileReader Reader = new FileReader(inputFile);
+		BufferedReader br = new BufferedReader(Reader);
+				
+		//si on met pas Intefer.valueOf() la ligne sera lu en String donc pour convertir en int 
+		int w = Integer.valueOf(br.readLine()); //lire première ligne pour récuperer la largeur 
+		int h = Integer.valueOf(br.readLine()); //lire la seconde ligne pour récupérer la hauteur 
+		
+		//lire les types et les orientations de pièce
+		//en cours de construction
+		
+		Grid grid = new Grid(w, h); //generer la grille avec w et h 
+		br.close();
+		
+		return grid;
 	}
 	
 	/**
@@ -32,6 +50,15 @@ public class Checker {
 	 * If all piece are connected it means that the game is solved
 	 */
 	public static boolean isSolution(String inputFile) throws IOException {
+		//en cours de construction
+		//utiliser isTotallyConnected dans grid 
+		/*for (Piece[] x : grid.getAllPieces()) {
+			for (Piece y :x) {
+				if (grid.isTotallyConnected(y) == true) {
+					return true; 
+			}
+		}
+	}*/
 		return false;
 		
 	}
