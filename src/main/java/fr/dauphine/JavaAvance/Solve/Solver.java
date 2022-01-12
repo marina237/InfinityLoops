8cf612c42076da7c218aa2c8597b096a600c76c4
Myriamchtf <myriamsara94@gmail.com>
2022-01-13 00:28:00 +0100

    Update Solver (Choice the piece to be tested)

--- src/main/java/fr/dauphine/JavaAvance/Solve/Solver.java
+++ src/main/java/fr/dauphine/JavaAvance/Solve/Solver.java
@@ -17,4 +17,111 @@ public class Solver {
 		// To be implemented
 
 	}
+	
+	/**
+	 * Choice the piece to be tested 
+	 * Fixer les pièces pour quelles ne soient pas testées 
+	 * @param grid
+	 * @param p
+	 */
+	//remplacer grid par input file 
+	//a revoir 
+	public static void PieceNotToBeTested (Grid grid, Piece p) {
+		if (p.isFixed() == false) {
+			//si la pièce est FOURCONN ou VOID, on la fixe car une seule position possible 
+			if (p.getType() == PieceType.FOURCONN || p.getType() == PieceType.VOID) {
+				p.setFixed(true);
+			}
+			else {
+				for (int line = 0; line < grid.getHeight(); line++) {
+					for (int column = 0; column < grid.getWidth(); column++) {
+						//coin en haut à gauche 
+						if (grid.isCorner(line, column) == true) {
+							if (line == 0 && column == 0) {
+								if (p.getType() == PieceType.LTYPE) {
+									p.setOrientation(1);
+									p.setFixed(true);
+								}
+							}	
+							//coin en haut à droite 
+							else if (line == 0 && column == grid.getWidth() -1 ) {
+								if (p.getType() == PieceType.LTYPE) {
+									p.setOrientation(2);
+									p.setFixed(true);
+								}
+							}
+							//coin en bas à gauche 
+							else if (line == grid.getHeight() -1 && column == 0) {
+								if (p.getType() == PieceType.LTYPE) {
+									p.setOrientation(0);
+									p.setFixed(true);
+								}
+							}
+							//coin en bas à droite 
+							else if (line == grid.getHeight() -1 && column == grid.getWidth() -1 ) {
+								if (p.getType() == PieceType.LTYPE) {
+									p.setOrientation(3);
+									p.setFixed(true);
+								}
+							}
+						}
+						else if (grid.isBorderLine(line, column) == true) {
+							//premiere ligne 
+							if (line == 0) {
+								if (p.getType() == PieceType.TTYPE) {
+									p.setOrientation(2);
+									p.setFixed(true);
+								}
+								else if (p.getType() == PieceType.BAR) {
+									p.setOrientation(1);
+									p.setFixed(true);
+								}
+							}
+							//derniere ligne
+							else if (line == grid.getHeight() - 1) {
+								if (p.getType() == PieceType.TTYPE) {
+									p.setOrientation(0);
+									p.setFixed(true);
+								}
+								else if (p.getType() == PieceType.BAR) {
+									p.setOrientation(1);
+									p.setFixed(true);
+								}
+							}
+						}
+						else if (grid.isBorderColumn(line, column) == true) {
+							//premiere colonne 
+							if (column == 0) {
+								if (p.getType() == PieceType.TTYPE) {
+									p.setOrientation(1);
+									p.setFixed(true);
+								}
+								else if (p.getType() == PieceType.BAR) {
+									p.setOrientation(0);
+									p.setFixed(true);
+								}
+							}
+							//dernire colonne
+							else if (column == grid.getWidth() -1) {
+								if (p.getType() == PieceType.TTYPE) {
+									p.setOrientation(3);
+									p.setFixed(true);
+								}
+								else if (p.getType() == PieceType.BAR) {
+									p.setOrientation(0);
+									p.setFixed(true);
+								}
+							}
+							
+						}	
+						
+					}
+
+				}
+				
+			}
+			
+		}
+		
+	}
 }
