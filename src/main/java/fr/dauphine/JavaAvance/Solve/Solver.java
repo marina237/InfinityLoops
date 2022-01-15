e18340dfdf2416b8ee9d8ff89890c07ad09c53ee
Myriamchtf <myriamsara94@gmail.com>
2022-01-15 22:34:40 +0100

    Update Solver (method PieceNotToBeTestedIfNeighborsAreFixed)

--- src/main/java/fr/dauphine/JavaAvance/Solve/Solver.java
+++ src/main/java/fr/dauphine/JavaAvance/Solve/Solver.java
@@ -158,22 +158,6 @@ public class Solver {
 		// To be implemented
 		
 		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
 
 	}
 	
@@ -282,5 +266,498 @@ public class Solver {
 			
 		}
 		
+	} // fin de piece not to be tested 
+	
+	public static void PieceNotToBeTestedIfNeighborsAreFixed (Grid grid, Piece p) {
+		
+		Piece leftNeighbor = grid.leftNeighbor(p) ;
+		Piece rightNeighbor = grid.rightNeighbor(p);
+		Piece topNeighbor = grid.topNeighbor(p);
+		Piece bottomNeighbor = grid.bottomNeighbor(p) ;
+		
+		for (int line = 0; line < grid.getWidth(); line ++) {
+			for (int column = 0; column < grid.getHeight(); column++) {
+
+				
+				if (line == 0 && column == 0) {
+					if(p.getType() == PieceType.LTYPE) {
+						if(rightNeighbor.isFixed() == false) {
+							if(rightNeighbor.getType() == PieceType.ONECONN) {
+								rightNeighbor.setOrientation(3);
+								rightNeighbor.setFixed(true);
+							}
+							else if (rightNeighbor.getType() == PieceType.LTYPE) {
+								rightNeighbor.setOrientation(2);
+								rightNeighbor.setFixed(true);
+							}
+						}else if (bottomNeighbor.isFixed() == false) {
+							if(bottomNeighbor.getType() == PieceType.ONECONN || bottomNeighbor.getType() == PieceType.LTYPE) {
+								bottomNeighbor.setOrientation(0);
+								bottomNeighbor.setFixed(true);
+							}
+						}
+					}else if (p.getType() == PieceType.VOID) {
+						if(rightNeighbor.isFixed() == false) {
+							if(rightNeighbor.getType() == PieceType.LTYPE) {
+								rightNeighbor.setOrientation(1);
+								rightNeighbor.setFixed(true);
+							}
+						} else if(bottomNeighbor.isFixed() == false) {
+							bottomNeighbor.setOrientation(1);
+							bottomNeighbor.setFixed(true);
+						}
+					}
+				}
+				
+				//corner premiere ligne derniere colonne
+				else if(line == 0 && column == grid.getWidth() -1) {
+					if(p.getType() == PieceType.LTYPE) {
+						if(leftNeighbor.isFixed()== false) {
+							if(leftNeighbor.getType() == PieceType.LTYPE) {
+								leftNeighbor.setOrientation(1);
+								leftNeighbor.setFixed(true);
+							} else if(leftNeighbor.getType() == PieceType.ONECONN) {
+								leftNeighbor.setOrientation(1);
+								leftNeighbor.setFixed(true);
+							}
+						}else if(bottomNeighbor.isFixed() == false) {
+							if(bottomNeighbor.getType() == PieceType.LTYPE) {
+								bottomNeighbor.setOrientation(3);
+								bottomNeighbor.setFixed(true);
+							}else if(bottomNeighbor.getType() == PieceType.ONECONN) {
+								bottomNeighbor.setOrientation(0);
+								bottomNeighbor.setFixed(true);
+							}
+						}
+					} 
+					else if (p.getType() == PieceType.VOID) {
+						if(leftNeighbor.isFixed() == false) {
+							if(leftNeighbor.getType() == PieceType.LTYPE) {
+								leftNeighbor.setOrientation(2);
+								leftNeighbor.setFixed(true);
+							}
+						}
+					}
+				}
+				
+				
+				//premier colonne derniere ligne
+				else if(column == 0 && line == grid.getHeight() -1) {
+					if(p.getType() == PieceType.LTYPE) {
+						if(topNeighbor.isFixed() == false) {
+							if(topNeighbor.getType() == PieceType.ONECONN) {
+								topNeighbor.setOrientation(2);
+								topNeighbor.setFixed(true);
+							}
+							if(topNeighbor.getType() == PieceType.LTYPE) {
+								topNeighbor.setOrientation(1);
+								topNeighbor.setFixed(true);
+							}
+						}
+						else if(rightNeighbor.isFixed() == false) {
+							if(rightNeighbor.getType() == PieceType.ONECONN || rightNeighbor.getType() == PieceType.LTYPE) {
+								rightNeighbor.setOrientation(3);
+								rightNeighbor.setFixed(true);
+							}
+						}
+					}
+					else if (p.getType() == PieceType.VOID) {
+						if(rightNeighbor.isFixed() == false) {
+							if(rightNeighbor.getType() == PieceType.LTYPE) {
+								rightNeighbor.setOrientation(0);
+								rightNeighbor.setFixed(true);
+							}
+						}
+					}
+				}
+				
+				
+				//dernier colonne et derniere ligne 
+				else if(column == grid.getWidth() -1 && line == grid.getHeight() -1) {
+					if(p.getType() == PieceType.LTYPE) {
+						if(topNeighbor.isFixed() == false) {
+							if(topNeighbor.getType() == PieceType.LTYPE || topNeighbor.getType() == PieceType.ONECONN) {
+								topNeighbor.setOrientation(2);
+								topNeighbor.setFixed(true);
+							}
+						}
+						else if(leftNeighbor.isFixed() == false) {
+							if(leftNeighbor.getType() == PieceType.ONECONN) {
+								leftNeighbor.setOrientation(1);
+								leftNeighbor.setOrientation(0);
+							}
+						}
+					}
+				}
+				
+				//BORDER LINE TOP
+				else if(line == 0 && column > 0 && column < grid.getWidth() -1) {
+					if(p.getType() == PieceType.TTYPE) {
+						if(leftNeighbor.isFixed() == false) {
+							if(leftNeighbor.getType() == PieceType.ONECONN || rightNeighbor.getType() == PieceType.LTYPE) {
+								leftNeighbor.setOrientation(1);
+								leftNeighbor.setFixed(true);
+							}
+						}
+						else if(rightNeighbor.isFixed() == false) {
+							if(rightNeighbor.getType() == PieceType.ONECONN) {
+								rightNeighbor.setOrientation(3);
+								rightNeighbor.setFixed(true);
+							}
+							else if(rightNeighbor.getType() == PieceType.LTYPE) {
+								rightNeighbor.setOrientation(2);
+								rightNeighbor.setFixed(true);
+							}
+						}
+						else if(bottomNeighbor.isFixed() == false) {
+							if(bottomNeighbor.getType() == PieceType.ONECONN || bottomNeighbor.getType() == PieceType.BAR) {
+								bottomNeighbor.setOrientation(0);
+								bottomNeighbor.setFixed(true);
+							}
+						}
+					}
+					else if(p.getType() == PieceType.BAR) {
+						if(leftNeighbor.isFixed() == false) {
+							if(leftNeighbor.getType() == PieceType.ONECONN || leftNeighbor.getType() == PieceType.LTYPE) {
+								leftNeighbor.setOrientation(1);
+								leftNeighbor.setFixed(true);
+							}
+						}
+						else if (rightNeighbor.isFixed() == false) {
+							if(rightNeighbor.getType() == PieceType.ONECONN) {
+								rightNeighbor.setOrientation(3);
+								rightNeighbor.setFixed(true);
+							}
+							else if(rightNeighbor.getType() == PieceType.LTYPE) {
+								rightNeighbor.setOrientation(2);
+								rightNeighbor.setFixed(true);
+							}
+						}
+						else if(bottomNeighbor.isFixed() == false) {
+							if(bottomNeighbor.getType() == PieceType.LTYPE) {
+								bottomNeighbor.setOrientation(2);
+								bottomNeighbor.setFixed(true);
+							}
+							else if(bottomNeighbor.getType() == PieceType.BAR) {
+								bottomNeighbor.setOrientation(1);
+								bottomNeighbor.setFixed(true);
+							}
+						}
+					}
+					else if(p.getType() == PieceType.VOID) {
+						if (bottomNeighbor.isFixed() == false) {
+							if(bottomNeighbor.getType() == PieceType.BAR) {
+								bottomNeighbor.setOrientation(1);
+								bottomNeighbor.setFixed(true);
+							}
+							else if(bottomNeighbor.getType() == PieceType.TTYPE) {
+								bottomNeighbor.setOrientation(2);
+								bottomNeighbor.setFixed(true);
+								}
+							}
+						else if(leftNeighbor.isFixed() == false) {
+							if(leftNeighbor.getType() == PieceType.LTYPE) {
+								leftNeighbor.setOrientation(2);
+								leftNeighbor.setFixed(true);
+							}
+						}
+						else if(rightNeighbor.isFixed() == false) {
+							if(rightNeighbor.getType() == PieceType.LTYPE) {
+								rightNeighbor.setOrientation(1);
+								rightNeighbor.setFixed(true);
+							}
+						}
+					}
+				}
+				else if(column == 0 && line > 0 && line < grid.getHeight() -1) {
+					if(p.getType() == PieceType.BAR) {
+						if(topNeighbor.isFixed() == false) {
+							if(topNeighbor.getType() == PieceType.ONECONN) {
+								topNeighbor.setOrientation(2);
+								topNeighbor.setFixed(true);
+							}
+							else if (topNeighbor.getType() == PieceType.LTYPE) {
+								topNeighbor.setOrientation(1);
+								topNeighbor.setFixed(true);
+							}
+						}
+						else if(bottomNeighbor.isFixed() == false) {
+							if(bottomNeighbor.getType() == PieceType.ONECONN || bottomNeighbor.getType() == PieceType.LTYPE) {
+								bottomNeighbor.setOrientation(0);
+								bottomNeighbor.setFixed(true);
+							}
+						}
+						else if(rightNeighbor.isFixed() == false) {
+							if(rightNeighbor.getType() == PieceType.BAR) {
+								rightNeighbor.setOrientation(0);
+								rightNeighbor.setFixed(true);
+							}
+							else if(rightNeighbor.getType() == PieceType.TTYPE) {
+								rightNeighbor.setOrientation(1);
+								rightNeighbor.setFixed(true);
+							}
+						}
+						
+					}
+					else if(p.getType() == PieceType.TTYPE) {
+						if(topNeighbor.isFixed() == false) {
+							if(topNeighbor.getType() == PieceType.ONECONN) {
+								topNeighbor.setOrientation(2);
+								topNeighbor.setFixed(true);
+							}
+							else if(topNeighbor.getType() == PieceType.LTYPE) {
+								topNeighbor.setOrientation(1);
+								topNeighbor.setFixed(true);
+							}
+						}
+						else if(bottomNeighbor.isFixed() == false) {
+							if(bottomNeighbor.getType() == PieceType.ONECONN || bottomNeighbor.getType() == PieceType.LTYPE) {
+								bottomNeighbor.setOrientation(0);
+								bottomNeighbor.setFixed(true);
+							}
+						}
+						else if(rightNeighbor.isFixed() == false) {
+							if(rightNeighbor.getType() == PieceType.BAR) {
+								rightNeighbor.setOrientation(1);
+								rightNeighbor.setFixed(true);
+							}
+							else if(rightNeighbor.getType() == PieceType.ONECONN) {
+								rightNeighbor.setOrientation(3);
+								rightNeighbor.setFixed(true);
+							}
+						}	
+					}
+					else if(p.getType() == PieceType.VOID) {
+						if(topNeighbor.isFixed() == false) {
+							if(topNeighbor.getType() == PieceType.LTYPE) {
+								topNeighbor.setOrientation(0);
+								topNeighbor.setFixed(true);
+							}
+						}
+						else if(bottomNeighbor.isFixed() == false) {
+							if(bottomNeighbor.getType() == PieceType.LTYPE) {
+								bottomNeighbor.setOrientation(1);
+								bottomNeighbor.setFixed(true);
+							}
+						}
+						else if(rightNeighbor.isFixed() == false) {
+							if(rightNeighbor.getType() == PieceType.BAR) {
+								rightNeighbor.setOrientation(0);
+								rightNeighbor.setFixed(true);
+							}
+							else if(rightNeighbor.getType() == PieceType.TTYPE) {
+								rightNeighbor.setOrientation(1);
+								rightNeighbor.setFixed(true);
+							}
+						}
+					}
+				}//fin de leftborder
+				else if(column > 0 && column < grid.getWidth() -1 && line == grid.getHeight() -1) {
+					if(p.getType() == PieceType.TTYPE) {
+						if(rightNeighbor.isFixed() == false) {
+							if(rightNeighbor.getType() == PieceType.ONECONN || rightNeighbor.getType() == PieceType.LTYPE) {
+								rightNeighbor.setOrientation(3);
+								rightNeighbor.setFixed(true);
+							}
+					}
+					else if(topNeighbor.isFixed() == false) {
+						if(topNeighbor.getType() == PieceType.BAR) {
+							topNeighbor.setOrientation(0);
+							topNeighbor.setFixed(true);
+						}
+						else if(topNeighbor.getType() == PieceType.ONECONN) {
+							topNeighbor.setOrientation(2);
+							topNeighbor.setFixed(true);
+							}
+						}
+					else if(leftNeighbor.isFixed() == false) {
+						if(leftNeighbor.getType() == PieceType.ONECONN) {
+							leftNeighbor.setOrientation(1);
+							leftNeighbor.setFixed(true);
+						}
+						else if(leftNeighbor.getType() == PieceType.LTYPE) {
+							leftNeighbor.setOrientation(0);
+							leftNeighbor.setFixed(true);
+						}
+					}
+					}
+					else if(p.getType() == PieceType.BAR) {
+						if(rightNeighbor.isFixed() == false) {
+							if(rightNeighbor.getType() == PieceType.ONECONN || rightNeighbor.getType() == PieceType.LTYPE) {
+								rightNeighbor.setOrientation(3);
+								rightNeighbor.setFixed(true);
+							}
+						}
+						else if(leftNeighbor.isFixed() == false) {
+							if(leftNeighbor.getType() == PieceType.ONECONN) {
+								leftNeighbor.setOrientation(1);
+								leftNeighbor.setFixed(true);
+							}
+							else if(leftNeighbor.getType() == PieceType.LTYPE) {
+								leftNeighbor.setOrientation(0);
+								leftNeighbor.setFixed(true);
+							}
+						}
+						else if(topNeighbor.isFixed() == false) {
+							if(topNeighbor.getType() == PieceType.TTYPE) {
+								topNeighbor.setOrientation(0);
+								topNeighbor.setFixed(true);
+							}
+							else if(topNeighbor.getType() == PieceType.BAR) {
+								topNeighbor.setOrientation(1);
+								topNeighbor.setFixed(true);
+							}
+						}
+					}
+					else if(p.getType() == PieceType.VOID) {
+						if(rightNeighbor.isFixed() == false) {
+							if(rightNeighbor.getType() == PieceType.LTYPE) {
+								rightNeighbor.setOrientation(0);
+								rightNeighbor.setFixed(true);
+							}
+						}
+						else if(leftNeighbor.isFixed() == false) {
+							if(leftNeighbor.getType() == PieceType.LTYPE) {
+								leftNeighbor.setOrientation(3);
+								leftNeighbor.setFixed(true);
+							}
+						}
+						else if(topNeighbor.isFixed() == false) {
+							if(topNeighbor.getType() == PieceType.BAR) {
+								topNeighbor.setOrientation(1);
+								topNeighbor.setFixed(true);
+							}
+							else if(topNeighbor.getType() == PieceType.TTYPE) {
+								topNeighbor.setOrientation(0);
+								topNeighbor.setFixed(true);
+							}
+						}
+					}
+				}// fin de bottom border
+				else if(line > 0 && line < grid.getHeight() -1 && column == grid.getWidth() -1) {
+					if(p.getType() == PieceType.TTYPE) {
+						if(leftNeighbor.isFixed() == false) {
+							if(leftNeighbor.getType() == PieceType.BAR || leftNeighbor.getType() == PieceType.ONECONN) {
+								leftNeighbor.setOrientation(1);
+								leftNeighbor.setFixed(true);
+							}
+						}
+						if(bottomNeighbor.isFixed() == false) {
+							if(bottomNeighbor.getType() == PieceType.LTYPE) {
+								bottomNeighbor.setOrientation(3);
+								bottomNeighbor.setFixed(true);
+							}
+							else if(bottomNeighbor.getType() == PieceType.ONECONN) {
+								bottomNeighbor.setOrientation(0);
+								bottomNeighbor.setFixed(true);
+							}
+						}
+						if(topNeighbor.isFixed() == false) {
+							if(topNeighbor.getType() == PieceType.ONECONN || topNeighbor.getType() == PieceType.LTYPE) {
+								topNeighbor.setOrientation(2);
+								topNeighbor.setFixed(true);
+							}
+						}
+					}
+					else if(p.getType() == PieceType.BAR) {
+						if(leftNeighbor.isFixed() == false) {
+							if(leftNeighbor.getType() == PieceType.BAR) {
+								leftNeighbor.setOrientation(0);
+								leftNeighbor.setFixed(true);
+							}
+							else if(leftNeighbor.getType() == PieceType.TTYPE) {
+								leftNeighbor.setOrientation(3);
+								leftNeighbor.setFixed(true);
+							}
+						}
+						if(bottomNeighbor.isFixed() == false) {
+							if(bottomNeighbor.getType() == PieceType.ONECONN) {
+								bottomNeighbor.setOrientation(0);
+								bottomNeighbor.setFixed(true);
+							}
+							else if(bottomNeighbor.getType() == PieceType.LTYPE) {
+								bottomNeighbor.setOrientation(3);
+								bottomNeighbor.setFixed(true);
+							}
+						}
+						if(topNeighbor.isFixed() == false) {
+							if(topNeighbor.getType() == PieceType.ONECONN || topNeighbor.getType() == PieceType.LTYPE) {
+								topNeighbor.setOrientation(2);
+								topNeighbor.setFixed(true);
+							}
+						}
+					}
+					else if(p.getType() == PieceType.VOID) {
+						if(leftNeighbor.isFixed() == false) {
+							if(leftNeighbor.getType() == PieceType.BAR) {
+								leftNeighbor.setOrientation(0);
+								leftNeighbor.setFixed(true);
+							}
+							else if(leftNeighbor.getType() == PieceType.LTYPE) {
+								leftNeighbor.setOrientation(3);
+								leftNeighbor.setFixed(true);
+							}
+						}
+						if(bottomNeighbor.isFixed() == false) {
+							if(bottomNeighbor.getType() == PieceType.LTYPE) {
+								bottomNeighbor.setOrientation(2);
+								bottomNeighbor.setFixed(true);
+							}
+						}
+						if(topNeighbor.isFixed() == false) {
+							if(topNeighbor.getType() == PieceType.LTYPE) {
+								topNeighbor.setOrientation(3);
+								topNeighbor.setFixed(true);
+							}
+						}
+					}
+				}//Fin de right border
+				//middle
+				else if(line > 0  && line < grid.getHeight() -1 && column > 0 && column < grid.getWidth() -1) {
+					if(p.getType() == PieceType.VOID) {
+						if(topNeighbor.isFixed() == false) {
+							if(topNeighbor.getType() == PieceType.BAR) {
+								topNeighbor.setOrientation(1);
+								topNeighbor.setFixed(true);
+							}
+							else if(topNeighbor.getType() == PieceType.TTYPE) {
+								topNeighbor.setOrientation(0);
+								topNeighbor.setFixed(true);
+							}
+						}
+						else if(bottomNeighbor.isFixed() == false) {
+							if(bottomNeighbor.getType() == PieceType.BAR) {
+								bottomNeighbor.setOrientation(1);
+								bottomNeighbor.setFixed(true);
+							}
+							else if(bottomNeighbor.getType() == PieceType.TTYPE) {
+								bottomNeighbor.setOrientation(2);
+								bottomNeighbor.setFixed(true);
+							}
+						}
+						else if(leftNeighbor.isFixed() == false) {
+							if(leftNeighbor.getType() == PieceType.TTYPE) {
+								leftNeighbor.setOrientation(3);
+								leftNeighbor.setFixed(true);
+							}
+							else if(leftNeighbor.getType() == PieceType.BAR) {
+								leftNeighbor.setOrientation(0);
+								leftNeighbor.setFixed(true);
+							}
+						}
+						else if(rightNeighbor.isFixed() == false) {
+							if(rightNeighbor.getType() == PieceType.TTYPE) {
+								leftNeighbor.setOrientation(1);
+								leftNeighbor.setFixed(true);
+							}
+							else if(rightNeighbor.getType() == PieceType.BAR) {
+								leftNeighbor.setOrientation(0);
+								leftNeighbor.setFixed(true);
+							}
+						}
+					}
+				}//fin de center
+			}
+		}
 	}
 }
