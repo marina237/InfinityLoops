3cdf966550f04becc818cdf75906580d6dbf4a3a
marina237 <mary-95320@hotmail.fr>
2022-01-15 23:56:22 +0100

    Test

---
+++ src/main/java/fr/dauphine/java/infinity/test/testJunit4.java
@@ -0,0 +1,28 @@
+package fr.dauphine.java.infinity.test;
+
+
+
+
+
+import org.junit.Test;
+
+
+
+
+
+
+public class testJunit4 {
+
+	@Test
+	
+	 void testSimple() {
+		
+
+	}
+	
+	
+	public testJunit4() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
